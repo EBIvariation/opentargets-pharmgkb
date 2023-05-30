d46ed1a1a4120e455b57ef790a7c738b9d81534a
April Shen <ashen@ebi.ac.uk>
2023-05-30 15:46:01 +0100

    add publications to evidence string

--- bin/generate_evidence.py
+++ bin/generate_evidence.py
@@ -6,6 +6,7 @@ from opentargets_pharmgkb import evidence_generation
 parser = argparse.ArgumentParser('Generates Open Targets evidence strings from PharmGKB data')
 parser.add_argument('--clinical-annot-path', help='Path to clinical_annotations.tsv', required=True)
 parser.add_argument('--clinical-alleles-path', help='Path to clinical_ann_alleles.tsv', required=True)
+parser.add_argument('--clinical-evidence-path', help='Path to clinical_ann_evidence.tsv', required=True)
 parser.add_argument('--drugs-path', help='Path to drugs.tsv', required=True)
 parser.add_argument('--created-date', help='Created date of downloaded files (provided by PharmGKB)', required=True)
 parser.add_argument('--output-path', help='Path to output evidence strings', required=True)
@@ -16,6 +17,7 @@ if __name__ == '__main__':
     evidence_generation.pipeline(
         clinical_annot_path=args.clinical_annot_path,
         clinical_alleles_path=args.clinical_alleles_path,
+        clinical_evidence_path=args.clinical_evidence_path,
         drugs_path=args.drugs_path,
         created_date=args.created_date,
         output_path=args.output_path
--- opentargets_pharmgkb/evidence_generation.py
+++ opentargets_pharmgkb/evidence_generation.py
@@ -8,40 +8,29 @@ from opentargets_pharmgkb.ols import get_chebi_iri
 from opentargets_pharmgkb.pandas_utils import none_to_nan, explode_column
 from opentargets_pharmgkb.variant_coordinates import get_coordinates_for_clinical_annotation
 
-
-column_names = [
-    'Clinical Annotation ID',
-    'Variant/Haplotypes',
-    'Gene',
-    'split_gene',
-    'ensembl_gene_id',
-    'Drug(s)',
-    'split_drug',
-    'chebi',
-    'Level of Evidence',
-    'Phenotype Category',
-    'Phenotype(s)',
-    'Genotype/Allele',
-    'all_genotypes',
-    'Annotation Text'
-]
+ID_COL_NAME = 'Clinical Annotation ID'
 
 
-def pipeline(clinical_annot_path, clinical_alleles_path, drugs_path, created_date, output_path):
+def pipeline(clinical_annot_path, clinical_alleles_path, clinical_evidence_path, drugs_path, created_date, output_path):
     clinical_annot_table = pd.read_csv(clinical_annot_path, sep='\t')
     clinical_alleles_table = pd.read_csv(clinical_alleles_path, sep='\t')
+    clinical_evidence_table = pd.read_csv(clinical_evidence_path, sep='\t')
     drugs_table = pd.read_csv(drugs_path, sep='\t')
 
-    merged_table = pd.merge(clinical_annot_table, clinical_alleles_table, on='Clinical Annotation ID', how='left')
+    merged_table = pd.merge(clinical_annot_table, clinical_alleles_table, on=ID_COL_NAME, how='left')
     # Restrict to variants with rsIDs
     rs_only_table = merged_table[merged_table['Variant/Haplotypes'].str.contains('rs')]
     # Also provide a column with all genotypes for a given rs
-    rs_only_table = pd.merge(rs_only_table, rs_only_table.groupby(by='Clinical Annotation ID').aggregate(
-        all_genotypes=('Genotype/Allele', list)), on='Clinical Annotation ID')
+    rs_only_table = pd.merge(rs_only_table, rs_only_table.groupby(by=ID_COL_NAME).aggregate(
+        all_genotypes=('Genotype/Allele', list)), on=ID_COL_NAME)
 
     mapped_genes = explode_and_map_genes(rs_only_table)
     mapped_drugs = explode_and_map_drugs(mapped_genes, drugs_table)
-    evidence_table = mapped_drugs[column_names]
+
+    # Add clinical evidence with PMIDs
+    pmid_evidence = clinical_evidence_table[clinical_evidence_table['PMID'].notna()].astype({'PMID': 'int'})
+    evidence_table = pd.merge(mapped_drugs, pmid_evidence.groupby(by=ID_COL_NAME).aggregate(
+        publications=('PMID', list)), on=ID_COL_NAME)
 
     # Generate evidence
     evidence = [generate_clinical_annotation_evidence(created_date, row) for _, row in evidence_table.iterrows()]
@@ -110,8 +99,9 @@ def generate_clinical_annotation_evidence(created_date, row):
 
         # RECORD ATTRIBUTES
         'datatypeId': 'clinical_annotation',
-        'studyId': row['Clinical Annotation ID'],
+        'studyId': row[ID_COL_NAME],
         'evidenceLevel': row['Level of Evidence'],
+        'literature': [str(x) for x in row['publications']],
 
         # VARIANT ATTRIBUTES
         'variantId': vcf_full_coords,
@@ -132,5 +122,6 @@ def generate_clinical_annotation_evidence(created_date, row):
         'phenotypeFromSourceId': row['Phenotype(s)']  # TODO EFO - needs to be exploded & mapped
     }
     # Remove the attributes with empty values (either None or empty lists).
-    evidence_string = {key: value for key, value in evidence_string.items() if value and pd.notna(value)}
+    evidence_string = {key: value for key, value in evidence_string.items()
+                       if value and (isinstance(value, list) or pd.notna(value))}
     return evidence_string
