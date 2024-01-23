a7c800f9e4af42139a348dab348d3c20a30208d7
April Shen <ashen@ebi.ac.uk>
2024-01-23 13:52:30 +0000

    small refactor of exact CHEBI matches

--- opentargets_pharmgkb/ontology_apis.py
+++ opentargets_pharmgkb/ontology_apis.py
@@ -1,9 +1,8 @@
 import logging
-import os
 from functools import lru_cache
 
-import requests
 from cmat.trait_mapping.main import process_trait
+from cmat.trait_mapping.ols import get_uri_from_exact_match
 from cmat.trait_mapping.trait import Trait
 from requests import RequestException
 from retry import retry
@@ -23,25 +22,8 @@ oxo_targets = ['Orphanet', 'efo', 'hp', 'mondo']
 oxo_distance = 1
 
 
-@lru_cache
-@retry(exceptions=(ConnectionError, RequestException), tries=4, delay=2, backoff=1.2, jitter=(1, 3))
 def get_chebi_iri(drug_name):
-    chebi_search_url = os.path.join(OLS_API_ROOT, f'search?ontology=chebi&q={drug_name}&queryFields=label&exact=true')
-    response = requests.get(chebi_search_url)
-    response.raise_for_status()
-    data = response.json()
-    if 'response' in data:
-        results = data['response']['docs']
-        candidates = set()
-        for result in results:
-            # Check that we've found the drug exactly (strict case-insensitive string match)
-            if result['label'].lower() == drug_name.lower():
-                candidates.add(result['iri'])
-        # Only return a result if we can find it unambiguously
-        if len(candidates) == 1:
-            return candidates.pop()
-    logger.warning(f'Could not find a CHEBI IRI for {drug_name}')
-    return None
+    return get_uri_from_exact_match(drug_name, ontology='chebi')
 
 
 @lru_cache
